3343effe8ecebbb41453724fc2cadd7c213e58b8
Trojx <raojianxun@126.com>
2016-11-16 23:06:32 +0800

    第4节  使用 eventproxy 控制并发

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,48 @@
 var express = require('express');
 var supergent=require('superagent');
 var cheerio=require('cheerio');
+var url=require('url');
+var eventproxy=require('eventproxy');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  var cnodeUrl='https://cnodejs.org/';
   supergent.get('https://cnodejs.org/')
       .end(function (err, sres) {
         if(err){
           return next(err);
         }
+        var topicUrls = [];
         var $=cheerio.load(sres.text);
-        var items=[];
         $('#topic_list').find('.topic_title').each(function (idx, element) {
           var $element = $(element);
-          items.push({
-            title: $element.attr('title'),
-            href: $element.attr('href')
-          });
+            var href=url.resolve(cnodeUrl,$element.attr('href'));
+            topicUrls.push(href);
         });
-        res.send(items);
+        var ep=new eventproxy();
+          ep.after('topic_html',topicUrls.length,function (topics) {
+              topics=topics.map(function (topicPair) {
+                  var topicUrl = topicPair[0];
+                  var topicHtml = topicPair[1];
+                  var $ = cheerio.load(topicHtml);
+                  var newTopic={
+                      title: $('.topic_full_title').text().trim(),
+                      href: topicUrl,
+                      comment1: $('.reply_content').eq(0).text().trim()
+                  };
+                  return newTopic;
+              });
+              console.log('final;');
+              console.log(topics);
+          });
+          topicUrls.forEach(function (topicUrl) {
+            supergent.get(topicUrl)
+                .end(function (err, sres) {
+                    console.log('fetch ' + topicUrl + ' successful');
+                    ep.emit('topic_html', [topicUrl, sres.text]);
+                })
+          })
       })
 });
 
