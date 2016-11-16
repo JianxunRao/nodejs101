71fdf9047d7e59ffcece333431dc9f6ed68ea928
Trojx <raojianxun@126.com>
2016-11-16 22:18:08 +0800

    第2节  学习使用外部模块

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var utility=require('utility');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  q=req.query.q;
+  if(q==null){
+    res.send('empty param');
+    return;
+  }
+  md5Value=utility.md5(q);
+  res.send(md5Value);
 });
 
 module.exports = router;
