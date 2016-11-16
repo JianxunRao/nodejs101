bc4c4a593680b5111d54baae37d19d098676975f
Trojx <raojianxun@126.com>
2016-11-16 22:31:36 +0800

    第3节  使用 superagent 与 cheerio 完成简单爬虫

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,26 @@
 var express = require('express');
-var utility=require('utility');
+var supergent=require('superagent');
+var cheerio=require('cheerio');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  q=req.query.q;
-  if(q==null){
-    res.send('empty param');
-    return;
-  }
-  md5Value=utility.md5(q);
-  res.send(md5Value);
+  supergent.get('https://cnodejs.org/')
+      .end(function (err, sres) {
+        if(err){
+          return next(err);
+        }
+        var $=cheerio.load(sres.text);
+        var items=[];
+        $('#topic_list').find('.topic_title').each(function (idx, element) {
+          var $element = $(element);
+          items.push({
+            title: $element.attr('title'),
+            href: $element.attr('href')
+          });
+        });
+        res.send(items);
+      })
 });
 
 module.exports = router;
